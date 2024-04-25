834094181ccabcf1b10ae72ec684087f0d6c2178
Hira-Yuki <cjsdlf44@gmail.com>
2024-04-25 22:56:52 +0900

    검색 기능 추가

--- src/Layouts/Header/Search/Search.tsx
+++ src/Layouts/Header/Search/Search.tsx
@@ -1,6 +1,20 @@
+import { useRecoilValueLoadable } from 'recoil'
 import SearchButton from '../Atom/SearchButton'
+import { productsList } from '../../../store/products'
+import { useState } from 'react'
 
 const Search = () => {
+  const productList = useRecoilValueLoadable(productsList)
+  const [inputValue, setInputValue] = useState("")
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const filteredProducts = productList.state === 'hasValue' && inputValue.trim() !== '' ?
+    productList.contents.filter((product) =>
+      product.title.toLowerCase().includes(inputValue.toLowerCase())
+    ) : [];
 
   return (
     <div className="dropdown">
@@ -9,13 +23,22 @@ const Search = () => {
         type="text"
         placeholder="검색"
         className="fixed left-0 top-4 -z-10 opacity-0 sm:opacity-100 sm:static sm:flex w-full input input-ghost focus:outline-0 rounded-none sm:rounded bg-gray-300 dark:bg-gray-600 !text-gray-800 dark:!text-white sm:transform-none transition-all js-searchInput"
-        value=""
-        control-id="ControlID-3"
+        value={inputValue}
+        onChange={handleInput}
       />
       <ul
         className="!fixed left-0 sm:!absolute sm:top-14 menu flex-nowrap dropdown-content w-full sm:w-64 max-h-96 shadow text-base-content overflow-y-auto overflow-x-hidden bg-white dark:bg-gray-600"
       >
         {/* 검색된 항목이 나열될 곳 */}
+        {filteredProducts.map((product) => (
+          <li key={product.id}>
+            <button type="button" className="text-left js-searchedItem">
+              <span className="text-gray-600 dark:text-white line-clamp-2">
+                {product.title}
+              </span>
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   )
