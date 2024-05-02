bcf726b62211c00fdba1f785fe759814bed78da9
Hira-Yuki <cjsdlf44@gmail.com>
2024-05-02 23:50:34 +0900

    코드스플릿 적용

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,24 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import { memo } from "react";
-import Error from "../views/Error";
-import Index from "../views/Index";
-import Fashion from "../views/Fashion";
-import Accessory from "../views/Accessory";
-import Digital from "../views/Digital";
-import Cart from "../views/Cart";
-import ProductsDetail from "../components/products/ProductsDetail";
+import { Suspense, lazy, memo } from "react";
+const Error = lazy(() => import("../views/Error"));
+const Index = lazy(() => import("../views/Index"));
+const Fashion = lazy(() => import('../views/Fashion'));
+const Accessory = lazy(() => import("../views/Accessory"))
+const Digital = lazy(() => import("../views/Digital"))
+const Cart = lazy(() => import("../views/Cart"))
+const ProductsDetail = lazy(() => import("../components/products/ProductsDetail"))
+
 
 const Router = (): JSX.Element => {
   return (
-    <Routes>
-      <Route path="*" element={<Error />} />
-      <Route path="/" element={<Index />} />
-      <Route path="/product/:id" element={<ProductsDetail />} />
-      <Route path="/fashion" element={<Fashion />} />
-      <Route path="/accessory" element={<Accessory />} />
-      <Route path="/digital" element={<Digital />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Index />} />
+        <Route path="/product/:id" element={<ProductsDetail />} />
+        <Route path="/fashion" element={<Fashion />} />
+        <Route path="/accessory" element={<Accessory />} />
+        <Route path="/digital" element={<Digital />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Suspense>
   );
 };
 
